b080a969b08c6e8bf6045b8a47bc8bb8a9b9eb3d
PYTH@N <python911@protonmail.com>
2021-03-19 22:36:05 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,33 @@
+echo "Welcome "
+echo " "
+echo "You can login to the Virtualizor Admin Panel"
+echo "using your ROOT details at the following URL :"
+echo "https://$ip:4085/"
+echo "OR"
+echo "http://$ip:4084/"
+echo " "
+echo "Do you want null virtulizor now? Y/N"
+read licNULL
+# NULLING VIRTULIZOR 
+if ([ "$licNULL" == "y" ] || [ "$licNULL" == "y" ]); then	
+	echo "Wait while i will do the magic..."
+	wget -O p.sh https://raw.githubusercontent.com/python-911/virtulizor/main/patch.sh && chmod 777 p.sh ./p.sh
+	echo "Succesfully nulled virtulizor..."
+	echo "For everything else hit me on https://github.com/python-911"
+	echo "After update or reboot license may be invalid"
+	echo "You can patch the nulled version anytime by using below command"
+	echo "To use nulled type ./p.sh and enjoy..."
+	exit;
+fi
+
+# REVBOOOOT
+echo "You will need to reboot this machine to load the correct kernel"
+echo -n "Do you want to reboot now ? [y/N]"
+read rebBOOT
+
+echo "Nulled version is not for commericial use please buy license form Softaculous Virtualizor !"
+
+if ([ "$rebBOOT" == "Y" ] || [ "$rebBOOT" == "y" ]); then	
+	echo "The system is now being RESTARTED"
+	reboot;
+fi
