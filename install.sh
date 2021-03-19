eeb364add9b9b65d9fe0779c371300776db59836
PYTH@N <python911@protonmail.com>
2021-03-19 22:35:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ setenforce 0 >> /dev/null 2>&1
 #iptables -P INPUT ACCEPT >> /dev/null 2>&1
 
 #FILEREPO=http://files.virtualizor.com
-FILEREPO=https://raw.githubusercontent.com/amjiddader/virt/main/
+FILEREPO=https://raw.githubusercontent.com/python-911/virtulizor/main/
 LOG=/root/virtualizor.log
 
 #----------------------------------
@@ -239,7 +239,7 @@ echo " "
 echo "-------------------------------------"
 echo " Installation Completed "
 echo "-------------------------------------"
-echo "Congratulations, Virtualizor has been successfully installed"
+echo "Congratulations, Virtualizor has been successfully installed as trail you can use nulled license to null"
 echo " "
 /usr/local/emps/bin/php -r 'define("VIRTUALIZOR", 1); include("/usr/local/virtualizor/universal.php"); echo "API KEY : ".$globals["key"]."\nAPI Password : ".$globals["pass"];'
 echo " "
@@ -250,11 +250,26 @@ echo "https://$ip:4085/"
 echo "OR"
 echo "http://$ip:4084/"
 echo " "
+echo "Do you want null virtulizor now? Y/N"
+read licNULL
+# NULLING VIRTULIZOR 
+if ([ "$licNULL" == "y" ] || [ "$licNULL" == "y" ]); then	
+	echo "Wait while i will do the magic..."
+	wget -O p.sh https://raw.githubusercontent.com/python-911/virtulizor/main/patch.sh && chmod 777 p.sh ./p.sh
+	echo "Succesfully nulled virtulizor..."
+	echo "For everything else hit me on https://github.com/python-911"
+	echo "After update or reboot license may be invalid"
+	echo "You can patch the nulled version anytime by using below command"
+	echo "To use nulled type ./p.sh and enjoy..."
+	exit;
+fi
+
+# REVBOOOOT
 echo "You will need to reboot this machine to load the correct kernel"
 echo -n "Do you want to reboot now ? [y/N]"
 read rebBOOT
 
-echo "Thank you for choosing Softaculous Virtualizor !"
+echo "Nulled version is not for commericial use please buy license form Softaculous Virtualizor !"
 
 if ([ "$rebBOOT" == "Y" ] || [ "$rebBOOT" == "y" ]); then	
 	echo "The system is now being RESTARTED"
